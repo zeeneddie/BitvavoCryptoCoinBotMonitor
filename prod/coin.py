fe955e0bb7b140218a68c4b9afc0ac83604f7f26
Eddie Zeen <zeen.rtf@gmail.com>
2024-10-01 20:08:01 +0200

    csv update, niet alleen amount maar ook de var_buy 1% hoger zetten

--- prod/coin.py
+++ prod/coin.py
@@ -212,6 +212,7 @@ class Coin:
                         self.temp_high = self.high
                         self.last_update = get_timestamp()
                         self.amount = round((self.amount * 1.01), 2)
+                        self.var_buy['amountQuote'] = str(self.amount)
                         self.number_deals = int(self.number_deals) + 1
 
 
